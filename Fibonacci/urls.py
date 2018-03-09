5870308a67f3bdb2f2e007d68dbf0af2b5a7908d
cheng <cyang@divinsa.com>
2018-03-08 16:59:45 -0800

    tweak

--- Fibonacci/urls.py
+++ Fibonacci/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-#from Fibonacci import views
+from Fibonacci import views
 
 urlpatterns = [
     #url('Fibonacci/(?P<n>-?\d+)/$', views.get_Fibonacci_list, name='Fibonacci'),
